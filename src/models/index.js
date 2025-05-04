336d29dedfd83f55ef81b6e99bae0329865222d4
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-05 00:58:47 +0200

    añadido clientModel en index

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,9 @@
+const Client = require('./nosql/client')
+
 const models = {
     usersModel: require('./nosql/users'),
+    clientModel: require('./nosql/client'),
 
  }
+ 
  module.exports = models

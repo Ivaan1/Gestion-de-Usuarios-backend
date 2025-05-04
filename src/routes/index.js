8f0d3de3b29696d9d5d204d782b1159eb3f88b2b
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-04 19:38:47 +0200

    update index

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
 const express = require("express"); 
- // Importa fs (file system), que nos permite trabajar con archivos del sistema 
- // (en este caso, para leer los archivos en la carpeta routes).
+
 const fs = require("fs");
 
-const router = express.Router(); // Crea un enrutador de express que se encargará de manejar las rutas.
+const router = express.Router(); 
 
 const removeExtension = (fileName) => {
     // Esta función recibe un nombre de archivo (por ejemplo: 'users.js') y devuelve el nombre sin la extensión (en este caso 'users').
@@ -19,7 +18,7 @@ fs.readdirSync(__dirname)
         return name !== 'index';
     }).forEach((file) => {
         const name = removeExtension(file);
-        router.use('/' + name, require('./' + name));
+        router.use('/' + name, require('./' + name));  // http://localhost:5000/api/tracks
     });
 
 module.exports = router; // Exportamos el enrutador para que pueda ser utilizado en otros archivos, como 'app.js'.

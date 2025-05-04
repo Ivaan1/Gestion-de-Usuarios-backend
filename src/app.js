3507e9927219cf6be00e949dbb4a33a469ce48ed
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-04 18:35:58 +0200

    Configuracion app swager

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require("express")
-
 const cors = require("cors")
-
 const dbConnect = require('./config/mongo')
 
 //require('dotenv').config();
 require('dotenv').config();
 
 const app = express()
+
+
+const swaggerUi = require("swagger-ui-express")
+const swaggerSpecs = require("./docs/swagger")
+
 
 //Le decimos a la app de express() que use cors para evitar el error Cross-Domain (XD)
 app.use(cors())
@@ -16,6 +19,11 @@ app.use(express.json())
 
 
 app.use("/api", require("./routes")) //Lee routes/index.js por defecto
+
+app.use("/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpecs)
+   )
 
 const port = process.env.PORT || 3000
 

5f28e6c7485db1d5d71d77ac13107dc4a019ed23
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-05 16:45:15 +0200

    routes albaranes

--- src/routes/albaranes.js
+++ src/routes/albaranes.js
@@ -1,8 +1,20 @@
 const router = require('express').Router();
-const { createAlbaran } = require('../controllers/albaranes');
-const { validatorCreateAlbaran } = require('../validators/albaranes');
+const { createAlbaran, getAlbaranes, getAlbaran, generatePDF, downloadPDF, uploadSign } = require('../controllers/albaranes');
+const { validatorCreateAlbaran, validatorGetAlbaran } = require('../validators/albaranes');
 const authMiddleWare = require('../middleware/sessions');
+const { uploadMiddlewareMemory } = require("../utils/handleStorage")
 
 router.post('/', authMiddleWare,validatorCreateAlbaran, createAlbaran)
+
+router.get('/', authMiddleWare, getAlbaranes)
+
+router.patch('/pdf/:id', authMiddleWare, validatorGetAlbaran, generatePDF)
+router.get('/pdf/:id', authMiddleWare,validatorGetAlbaran, downloadPDF)
+
+router.patch('/sign/:id', authMiddleWare, validatorGetAlbaran, uploadMiddlewareMemory.single("image"), uploadSign)
+
+router.get('/:id', authMiddleWare, validatorGetAlbaran, getAlbaran)
+
+
 
 module.exports = router;

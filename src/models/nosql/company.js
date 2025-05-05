7668b4f5b34e9c31ae0241f86fea58b8a7720dcb
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-05 21:58:37 +0200

    crear model company

--- src/models/nosql/company.js
+++ src/models/nosql/company.js
@@ -1,21 +1,42 @@
 const mongoose = require('mongoose');
+// Dirección del cliente
+const addressSchema = new mongoose.Schema({
+    street: { // Calle
+        type: String,
+        required: true
+    },
+    number: { // Número de la dirección
+        type: Number,
+        required: true
+    },
+    postal: { // Código postal
+        type: Number,
+        required: true
+    },
+    city: { // Ciudad
+        type: String,
+        required: true
+    },
+    province: { // Provincia
+        type: String,
+        required: true
+    }
+}, { _id: false }) // No queremos un _id para la dirección
 
 const companySchema = new mongoose.Schema({
   name: { 
     type: String, 
-    required: true },
-  address: {
-    street: String,
-    number: Number,
-    postal: Number,
-    city: String,
-    province: String
-  },
+    required: true 
+    },
+    address: { // Dirección
+        type: addressSchema,
+        required: true
+    },
   users: {
     type: [mongoose.Schema.Types.ObjectId],
     ref: 'users',
-    required: true
+    required: false // Puede ser opcional si la empresa no tiene usuarios asociados
   }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Company', companySchema);
+module.exports = mongoose.model('companies', companySchema); 

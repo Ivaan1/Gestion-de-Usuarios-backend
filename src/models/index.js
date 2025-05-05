fdd8150deed1120b9ea6c078a9d9abb3316f489e
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-05 03:33:27 +0200

    models index implement project

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,9 @@
-const Client = require('./nosql/client')
 
+// Importar todos los modelos de la carpeta 'nosql'
 const models = {
     usersModel: require('./nosql/users'),
     clientModel: require('./nosql/client'),
-
+    projectModel: require('./nosql/project'),
  }
  
  module.exports = models

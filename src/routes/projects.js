696bcb8ff9ad373e493724a0104be149fc601906
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-05 03:46:12 +0200

    routes de project actualizado

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const { createProject, updateProject, getProjects } = require("../controllers/projects")
-const { validatorCreateProject, validatorUpdateProject } = require('../validators/projects')
+const { createProject, updateProject, getProjects, getProject, archiveProject, restoreProject, deleteProject } = require("../controllers/projects")
+const { validatorCreateProject, validatorUpdateProject, validatorGetProject } = require('../validators/projects')
 const authMiddleWare = require("../middleware/sessions")
 
 router.get("/", authMiddleWare, getProjects) 
+
 router.post("/", authMiddleWare, validatorCreateProject, createProject)
+
+router.patch("/archive/:id", authMiddleWare, validatorGetProject, archiveProject) 
+
+router.patch("/restore/:id", authMiddleWare, validatorGetProject, restoreProject)
+
+router.get("/:id", authMiddleWare, validatorGetProject, getProject)
+
 router.put("/:id", authMiddleWare, validatorUpdateProject, updateProject) 
+
+router.delete("/:id", authMiddleWare, validatorGetProject, deleteProject) // Eliminar un proyecto (soft delete)
 
 module.exports = router;

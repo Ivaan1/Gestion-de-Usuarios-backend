8925d92b41a18f58bb7754641c1e7216c7b1a098
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-05 21:59:51 +0200

    añadir validator add company

--- src/validators/users.js
+++ src/validators/users.js
@@ -23,5 +23,21 @@ const validatorUpdateUser = [
     (req, res, next) => validateResults(req, res, next)
 ];
 
+const validatorAddCompany = [
+    check("name").exists().notEmpty().withMessage("El nombre de la compañia es obligatorio"),
+    check("address").exists().notEmpty().withMessage("La dirección de la compañia es obligatoria"),
+    check("address.street").exists().notEmpty().withMessage("La calle de la compañia es obligatoria"),
+    check("address.number").exists().notEmpty().withMessage("El número de la compañia es obligatorio"),
+    check("address.postal").exists().notEmpty().withMessage("El código postal de la compañia es obligatorio"),
+    check("address.city").exists().notEmpty().withMessage("La ciudad de la compañia es obligatoria"),
+    check("address.province").exists().notEmpty().withMessage("La provincia de la compañia es obligatoria"),
+    (req, res, next) => validateResults(req, res, next)
+];
 
-module.exports = {validatorGetUser, validatorCreateUser, validatorUpdateUser}
+
+module.exports = {
+    validatorGetUser, 
+    validatorCreateUser, 
+    validatorUpdateUser,
+    validatorAddCompany,
+}

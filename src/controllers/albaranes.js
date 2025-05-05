22586542b7a3a54deb220832322766e0b1fd2c7f
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-05 19:25:18 +0200

    fix uploadSign y añadido deleteAlbaran

--- src/controllers/albaranes.js
+++ src/controllers/albaranes.js
@@ -3,6 +3,9 @@ const { handleHttpError } = require('../utils/handleErrors');
 const { matchedData } = require('express-validator')
 const { generateAlbaranPDF, saveAlbaranPDF } = require('../services/pdfGenerator'); 
 const { uploadToPinata } = require('../utils/handleUploadIPFS');
+const path = require('path');
+const fs = require('fs/promises');
+
 
 async function createAlbaran(req, res) {
     try {
@@ -101,6 +104,7 @@ async function getAlbaran(req, res) {
 async function generatePDF(req, res) {
     try {
         const { id } = req.params;
+        const { userAuthId } = req.user; // ID extraído del token 
     
         // Obtener datos del albarán de la base de datos
         const albaran = await albaranModel.findById(id)
@@ -111,11 +115,14 @@ async function generatePDF(req, res) {
         if (!albaran) {
             return handleHttpError(res, 'ALBARAN_NOT_FOUND', 404);
         }
-        
-        // Guardar el PDF en el sistema de archivos (opcional)
-        // Si prefieres no guardarlo en disco, puedes omitir esta línea
+        // El usuario solo podra acceder a sus albaranes 
+        if (albaran.userId._id.toString() !== userAuthId) {
+            return handleHttpError(res, 'UNAUTHORIZED_ACCESS', 403);
+        }
+
+        // Se guarda el PDF localmente en el servidor pero no se permite descargarlo sin firmarlo
         await saveAlbaranPDF(albaran);
-        
+
         // Configurar cabeceras para descarga del PDF
         res.setHeader('Content-Type', 'application/pdf');
         res.setHeader('Content-Disposition', `attachment; filename=albaran-${id}.pdf`);
@@ -132,35 +139,25 @@ async function generatePDF(req, res) {
 
 async function downloadPDF(req, res) {
     try {
-      const { id } = req.params;
+        const { id } = req.params;
       
-      // Verificar si el PDF ya existe en el sistema de archivos
-      const pdfPath = path.join(__dirname, '../uploads/albaranes', `albaran-${id}.pdf`);
+        const albaran = await albaranModel.findById(id);
       
-      if (fs.existsSync(pdfPath)) {
-        // Si el PDF ya existe, enviarlo como descarga
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader('Content-Disposition', `attachment; filename=albaran-${id}.pdf`);
-        return fs.createReadStream(pdfPath).pipe(res);
-      }
-      
-      // Si no existe, obtener datos del albarán
-      const albaran = await albaranModel.findById(id)
-        .populate('userId', 'name email address')
-        .populate('clientId', 'name address cif')
-        .populate('projectId', 'name projectCode');
-      
-      if (!albaran) {
-        return handleHttpError(res, 'ALBARAN_NOT_FOUND', 404);
-      }
-      
-      // Configurar cabeceras para descarga del PDF
-      res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', `attachment; filename=albaran-${id}.pdf`);
-      
-      // Generar el PDF directamente al response stream
-      await generateAlbaranPDF(albaran, res);
-      
+        if (!albaran) {
+            return handleHttpError(res, 'ALBARAN_NOT_FOUND', 404);
+        }
+        // Verificar si el albarán está firmado
+        if (!albaran.signed) {
+            return handleHttpError(res, 'ALBARAN_NOT_SIGNED', 400);
+        }
+        const pdfUrl = albaran.pdfUrl;
+        if (!pdfUrl) {
+            return handleHttpError(res, 'PDF_NOT_FOUND', 404);
+        }
+
+        // Redirigir a la URL de IPFS para la descarga
+        res.redirect(pdfUrl);
+
     } catch (error) {
       console.log(error);
       handleHttpError(res, 'ERROR_DOWNLOAD_ALBARAN_PDF', 500);
@@ -169,42 +166,84 @@ async function downloadPDF(req, res) {
 
   async function uploadSign(req, res) {
       try {
-        const { id } = req.params;
-        const body = matchedData(req); // Extraer los datos del cuerpo de la solicitud
+        const { id } = req.params; //ID del albaran 
+        const albaran = await albaranModel.findById(id); // Obtener el albarán por ID
 
+        
           if (!req.file) {
               handleHttpError(res, 'NO_FILE_UPLOADED', 400)
           }
-  
+
+            // Subir la firma a IPFS
           const fileBuffer = req.file.buffer;
           const fileName = req.file.originalname;
-          const pinataResponse = await uploadToPinata(fileBuffer, fileName);
 
+          ///subir la firma a pinata
+          const pinataResponse = await uploadToPinata(fileBuffer, fileName);
+          
           if (!pinataResponse || !pinataResponse.IpfsHash) {
             return handleHttpError(res, 'PINATA_UPLOAD_FAILED', 500);
         }
-  
+    
         const ipfsFile = pinataResponse.IpfsHash;
         const ipfsUrl = `https://${process.env.PINATA_GATEWAY_URL}/ipfs/${ipfsFile}`;
-
+        
         // Asignar la URL de la firma al cuerpo
-        body.sign = ipfsUrl;
+        albaran.sign = ipfsUrl;
+        albaran.signed = true; // Marcar el albarán como firmado
 
-        // Buscar el albarán y actualizarlo con la firma
-        const albaran = await albaranModel.findByIdAndUpdate(id, { $set: body }, { new: true });
+        //accedemos a la ruta del pdf que se guarda en el servidor
+        const pdfPath = path.join(__dirname, '../uploads/albaranes', `albaran-${id}.pdf`);
+        const pdfBuffer = await fs.readFile(pdfPath);
 
-        // Si no se encuentra el albarán
-        if (!albaran) {
-            return handleHttpError(res, 'ALBARAN_NOT_FOUND', 404);
-        }
+        const pdfFileName = `albaran-${id}.pdf`; // El nombre del archivo PDF
 
-        // Responder con el albarán actualizado
-        res.status(200).send({ data: albaran });
+       // Subir el PDF a Pinata
+       
+       const pdfPinataResponse = await uploadToPinata(pdfBuffer, pdfFileName);
+        
+       if (!pdfPinataResponse || !pdfPinataResponse.IpfsHash) {
+           return handleHttpError(res, 'PDF_PINATA_UPLOAD_FAILED', 500);
+       }
+
+       const pdfIpfsFile = pdfPinataResponse.IpfsHash;
+       const pdfIpfsUrl = `https://${process.env.PINATA_GATEWAY_URL}/ipfs/${pdfIpfsFile}`;
+
+       // Actualizar el albarán con la URL del PDF
+       albaran.pdfUrl = pdfIpfsUrl;
+       
+       // Actualizar el albarán en la base de datos
+       await albaran.save();
+
+       // Responder con el albarán actualizado
+       res.status(200).send({ data: albaran });
+
+       await fs.unlink(pdfPath); // Eliminar el PDF localmente después de subirlo a IPFS
+
       } catch (error) {
           console.error(error);
           handleHttpError(res, 'ERROR_UPLOAD_SIGN', 500);
       }
   }
+
+  async function deleteAlbaran(req, res) {
+    try {
+        const { id } = req.params; // ID del albarán a eliminar
+        const albaran = await albaranModel.findById(id); // Buscar albarán por ID
+        if (!albaran) {
+            return res.status(404).send('Albarán no encontrado');
+        }   
+        if (albaran.signed) {
+            return res.status(400).send('No se puede eliminar un albarán firmado');
+        }
+        await albaranModel.findByIdAndDelete(id); // Eliminar albarán
+        res.send({ message: 'Albarán eliminado' });
+    }
+    catch (error) {
+        console.log(error); // Imprimir el error en la consola
+        handleHttpError(res, 'ERROR_DELETE_ALBARAN', 500); // Manejar el error y enviar una respuesta al cliente
+    }
+}
 
 module.exports = {
     createAlbaran,
@@ -212,6 +251,7 @@ module.exports = {
     getAlbaran,
     generatePDF,
     downloadPDF,
-    uploadSign
+    uploadSign,
+    deleteAlbaran
     // Aquí puedes agregar más funciones para manejar otras operaciones relacionadas con albaranes
 };

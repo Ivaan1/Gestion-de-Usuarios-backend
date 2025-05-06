b5c74dc0c2440f0f8be9736250b678e089d797df
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-06 06:13:05 +0200

    configurar tests
    donde

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -1,33 +1,70 @@
 const request = require('supertest');
 const app = require('../app')
+
 describe('users', () => {
- var token = ""
- var id = ""
- it('should register a user' , async () => {
- const response = await request(app)
- .post('/api/auth/register' )
- .send({"name": "Menganito","age": 20,"email": "user25@test.com","password": "HolaMundo.01"})
- .set('Accept', 'application/json' )
- .expect(200)
- expect(response.body.user.email).toEqual('user25@test.com')
- expect(response.body.user.role).toEqual('user')
- token = response.body.token
- id = response.body.user._id
- })
- it('should get the users' , async () => {
- const response = await request(app)
- .get('/api/auth/users')
- .auth(token, { type: 'bearer' })
- .set('Accept', 'application/json' )
- .expect(200)
- expect(response.body.pop().name).toEqual('Menganito')
- });
- it('should delete a user' , async () => {
- const response = await request(app)
- .delete('/api/auth/users/' +id)
- .auth(token, { type: 'bearer' })
- .set('Accept', 'application/json' )
- .expect(200)
- expect(response.body.acknowledged).toEqual(true)
- })
+    var token = ""
+    var id = ""
+    var code = ""
+    it('should register a user' , async () => {
+        const response = await request(app)
+            .post('/api/auth/register' )
+            .send({"email": "user25@test.com","password": "HolaMundo.01"})
+            .set('Accept', 'application/json' )
+            .expect(200)
+        expect(response.body.user.email).toEqual('user25@test.com')
+        expect(response.body.user.role).toEqual('usuario')
+        token = response.body.token
+        id = response.body.user._id
+        code = parseInt(response.body.user.validationCode, 10);
+    })
+
+    it('should validate the user by validationCode', async () => {
+        const response = await request(app)
+            .post('/api/auth/validation')  
+            .send({"code": code})  
+            .set('Accept', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200);
+        
+        expect(response.body.email).toEqual('user25@test.com');
+        expect(response.body.validated).toEqual(true);
+        expect(response.body._id).toEqual(id);
+         
+    token = response.body.token;
+    id = response.body._id;
+    });
+
+    it('should sign in with the user ', async () => {
+        const response = await request(app)
+            .post('/api/auth/login')  
+            .send({"email": "user25@test.com","password": "HolaMundo.01"})
+            .set('Accept', 'application/json')
+            .expect(200);
+
+        expect(response.body.token).toBeDefined();
+        expect(typeof response.body.token).toBe('string');
+        expect(response.body.user._id).toEqual(id);
+        token = response.body.token;
+        id = response.body.user._id;
+    });
+
+    it('should get the users' , async () => {
+        const response = await request(app)
+            .get('/api/users')
+            .auth(token, { type: 'bearer' })
+            .set('Accept', 'application/json' )
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+        expect(response.body.pop().email).toEqual('user25@test.com')
+    });
+
+    it('should delete a user' , async () => {
+        const response = await request(app)
+            .delete('/api/users/' + id)
+            .auth(token, { type: 'bearer' })
+            .set('Accept', 'application/json' )
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200)
+        expect(response.body.acknowledged).toEqual(true)
+    })
 })

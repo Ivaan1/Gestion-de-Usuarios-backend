e4e53fa94c3b46a68256b5c6ed2a64d4e3267073
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-17 20:13:36 +0200

    validators auth

--- src/validators/auth.js
+++ src/validators/auth.js
@@ -2,8 +2,15 @@ const { check } = require("express-validator")
 const validateResults = require("../utils/handleValidator")
 
  const validatorRegister = [
-     check("email").exists().notEmpty().isEmail().withMessage("El correo es super obligatorio."),
-     check("password").exists().notEmpty().isLength({ min: 8, max: 16 }).withMessage("La contraseña es obligatoria."),
+    check("email")
+    .exists().withMessage("El correo es obligatorio.")
+    .notEmpty().withMessage("El correo no puede estar vacío.")
+    .isEmail().withMessage("El correo no tiene un formato válido."),
+    check("password")
+    .exists().withMessage("La contraseña es obligatoria.")
+    .notEmpty().withMessage("La contraseña no puede estar vacía.")
+    .isLength({ min: 8, max: 16 }).withMessage("La contraseña debe tener entre 8 y 16 caracteres."),
+  
      (req, res, next) => {
          return validateResults(req, res, next)
     }
@@ -11,30 +18,49 @@ const validateResults = require("../utils/handleValidator")
 
 
 const validatorLogin = [
-    check("email").exists().notEmpty().isEmail(),
-    check("password").isLength({ min: 8, max: 16 }).withMessage("La contraseña tiene que tener entre 8 y 16 caracteres"),
-    check("password").exists().notEmpty().withMessage("La contraseña es obligatoria"),
+    check("email")
+    .exists().withMessage("El correo es obligatorio.")
+    .notEmpty().withMessage("El correo no puede estar vacío.")
+    .isEmail().withMessage("El correo no tiene un formato válido."),
+
+  check("password")
+    .exists().withMessage("La contraseña es obligatoria.")
+    .notEmpty().withMessage("La contraseña no puede estar vacía.")
+    .isLength({ min: 8, max: 16 }).withMessage("La contraseña tiene que tener entre 8 y 16 caracteres"),
+
     (req, res, next) => validateResults(req, res, next)
 ]
 
 const validatorValidation = [
-    check("code").exists().notEmpty().isNumeric(),
+    check("code")
+    .exists().withMessage("El código es obligatorio.")
+    .notEmpty().withMessage("El código no puede estar vacío.")
+    .isNumeric().withMessage("El código debe ser numérico."),
+  
     (req, res, next) => validateResults(req, res, next)
 ]
 
 const validatorRecoverPassword = [
-    check("email").exists().notEmpty().isEmail().withMessage("Email required"),
-    check("email").isEmail().withMessage("Has to be valid email"),
+    check("email")
+    .exists().withMessage("El email es obligatorio.")
+    .notEmpty().withMessage("El email no puede estar vacío.")
+    .isEmail().withMessage("Debe ser un email válido."),
     (req, res, next) => validateResults(req, res, next)
 ]
 
 const validatorNewPassword = [
     check("code").optional(),
-    check("oldPassword").optional(),
-    check("newPassword1").exists().notEmpty().withMessage("La contraseña es obligatoria"),
-    check("newPassword1").isLength({ min: 8, max: 16 }).withMessage("La contraseña tiene que tener entre 8 y 16 caracteres"),
-    check("newPassword2").exists().notEmpty().withMessage("La contraseña es obligatoria"),
-    check("newPassword2").isLength({ min: 8, max: 16 }).withMessage("La contraseña tiene que tener entre 8 y 16 caracteres"),
+  check("oldPassword").optional(),
+
+  check("newPassword1")
+    .exists().withMessage("La contraseña es obligatoria.")
+    .notEmpty().withMessage("La contraseña no puede estar vacía.")
+    .isLength({ min: 8, max: 16 }).withMessage("Debe tener entre 8 y 16 caracteres."),
+
+  check("newPassword2")
+    .exists().withMessage("La contraseña es obligatoria.")
+    .notEmpty().withMessage("La contraseña no puede estar vacía.")
+    .isLength({ min: 8, max: 16 }).withMessage("Debe tener entre 8 y 16 caracteres."),
     (req, res, next) => validateResults(req, res, next)
 ]
 

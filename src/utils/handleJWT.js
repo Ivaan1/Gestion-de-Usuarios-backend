b1dadc06354e3e5fdbbefe73f9213d1757809afe
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-19 23:34:08 +0200

    recoverytoken implements

--- src/utils/handleJWT.js
+++ src/utils/handleJWT.js
@@ -11,7 +11,7 @@ function tokenSign(user){
         },
         JWT_SECRET,
         {
-            expiresIn: "120d" //TODO: cambiar a 7
+            expiresIn: "120d" //cambiar a 7
         }
     )
     return sign
@@ -25,4 +25,34 @@ function verifyToken(tokenJwt){
     }
 }
 
-module.exports = {tokenSign, verifyToken}
+
+function tokenSignRecovery(user){
+    return jwt.sign(
+        {
+            _id: user._id,
+            purpose: "password_reset"
+        },
+        JWT_SECRET,
+        {
+            expiresIn: "15m" // corto para seguridad
+        }
+    );
+}
+
+function verifyRecoveryToken(tokenJwt){
+    try {
+        const payload = jwt.verify(tokenJwt, JWT_SECRET);
+        if(payload.purpose !== "password_reset") throw new Error("Token inválido para recuperación");
+        return payload;
+    } catch(e){
+        console.log(e);
+        return null;
+    }
+}
+
+module.exports = {
+    tokenSign, 
+    verifyToken,
+    tokenSignRecovery,
+    verifyRecoveryToken
+    }

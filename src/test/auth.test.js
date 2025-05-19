e27bd187e179818e8e97187c3d99c0d63ed00277
Ivan <80658123+Ivaan1@users.noreply.github.com>
2025-05-19 23:33:58 +0200

    auth jest

--- src/test/auth.test.js
+++ src/test/auth.test.js
@@ -12,10 +12,9 @@ describe('users', () => {
             .set('Accept', 'application/json' )
             .expect(200)
         expect(response.body.user.email).toEqual('user25@test.com')
-        expect(response.body.user.role).toEqual('usuario')
         token = response.body.token
-        id = response.body.user._id
-        code = parseInt(response.body.user.validationCode, 10);
+        code = response.body.user.validationCode
+
     })
 
     it('should validate the user by validationCode', async () => {
@@ -26,12 +25,8 @@ describe('users', () => {
             .set('Authorization', `Bearer ${token}`)
             .expect(200);
         
-        expect(response.body.email).toEqual('user25@test.com');
-        expect(response.body.validated).toEqual(true);
-        expect(response.body._id).toEqual(id);
+        expect(response.body.user.validated).toEqual(true);
          
-    token = response.body.token;
-    id = response.body._id;
     });
 
     it('should sign in with the user ', async () => {
@@ -41,24 +36,68 @@ describe('users', () => {
             .set('Accept', 'application/json')
             .expect(200);
 
+        expect(response.body.token).toBeDefined();
+        expect(typeof response.body.token).toBe('string');
+        expect(response.body.user._id).toBeDefined();
+
+        token = response.body.token;
+
+    });
+
+    it('should recover password, should get a new validationcode' , async () => {
+        const response = await request(app)
+            .post('/api/auth/recovery')  
+            .send({"email": "user25@test.com"})
+            .set('Accept', 'application/json')
+            .expect(200);
+
+        token = response.body.token;
+        code = response.body.code;
+
+        expect(token).toBeDefined();
+        expect(typeof token).toBe('string');
+        expect(code).toBeDefined();
+        expect(typeof code).toBe('number');
+    })
+
+    it('should validate with recovery code', async () => {
+        const response = await request(app)
+            .post('/api/auth/validation')  
+            .send({"code": code})  
+            .set('Accept', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200);
+        
+        expect(response.body.user.validated).toEqual(true);
+        expect(response.body.token).toBeDefined();
+        expect(typeof response.body.token).toBe('string');
+        token = response.body.token;
+    });
+
+    it('should change the password' , async () => {
+        const response = await request(app)
+            .patch('/api/auth/newpassword')  
+            .send({"newPassword1": "HolaMundo.02", "newPassword2": "HolaMundo.02"})  
+            .set('Accept', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .expect(200);
+
+        expect(response.body.message).toEqual('Contraseña actualizada');
+    });
+
+    it('should sign in with the new password ', async () => {
+        const response = await request(app)
+            .post('/api/auth/login')  
+            .send({"email": "user25@test.com","password": "HolaMundo.02"})
+            .set('Accept', 'application/json')
+            .expect(200);
+
         token = response.body.token;
         id = response.body.user._id;
 
         expect(token).toBeDefined();
         expect(typeof token).toBe('string');
         expect(id).toBeDefined();
-        
-    });
-
-    it('should get the users' , async () => {
-        const response = await request(app)
-            .get('/api/users')
-            .auth(token, { type: 'bearer' })
-            .set('Accept', 'application/json' )
-            .set('Authorization', `Bearer ${token}`)
-            .expect(200)
-        expect(response.body.some(u => u.email === 'user25@test.com')).toBe(true);
-
         
     });
 

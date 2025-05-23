e9f1b72d082405a23387bb2c10ab2f56328be863
Ivan <ivanmanuel121310@gmail.com>
2025-05-23 16:29:16 +0200

    implements endpoint delete all

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 // Controladores para manejar las rutas
-const { getClients, getClient, createClient, archiveClient, getArchivedClients, deleteClient, restoreClient, updateClient, getProjectOfClient } = require("../controllers/clients")
+const { getClients, getClient, createClient, archiveClient, getArchivedClients, deleteClient, restoreClient, updateClient, getProjectOfClient, deleteAllClients } = require("../controllers/clients")
 const { validatorCreateClient, validatorGetClient, validatorUpdateClient } = require('../validators/clients')
 const authMiddleWare = require("../middleware/sessions")
 const { validateUserMiddleware } = require("../middleware/validated")
@@ -12,6 +12,9 @@ router.get("/", authMiddleWare,validateUserMiddleware, getClients)
 
 // CREAR un cliente
 router.post("/", authMiddleWare, validatorCreateClient,validateUserMiddleware, createClient)
+
+//SOLO USAR PARA PRUEBAS
+router.delete("/", deleteAllClients);
 
 // obtener TODOS los clientes ARCHIVADOS relacionados a la cuenta de usuario
 router.get("/archive", authMiddleWare, getArchivedClients)
@@ -33,5 +36,7 @@ router.get("/:id", authMiddleWare, validatorGetClient,validateUserMiddleware, ge
 
 //OBTENER los PROYECTOS de un CLIENTE
 router.get("/:id/projects", authMiddleWare, validatorGetClient,validateUserMiddleware, getProjectOfClient)
+
+
 
 module.exports = router;
